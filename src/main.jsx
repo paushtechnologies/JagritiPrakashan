47cc5852428b82cb2541d12371ea79146661425a
paushtechnologies <paushtechnologies@gmail.com>
2025-12-24 04:37:48 +0530

    custom domain addition

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-// src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-const root = createRoot(document.getElementById("root"));
-root.render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter>   {/* ❌ REMOVE basename */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
