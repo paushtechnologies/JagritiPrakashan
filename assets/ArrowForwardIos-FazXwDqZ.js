d389f4f480d77321e28c3f95454992087b8fa0e2
paushtechnologies <paushtechnologies@gmail.com>
2025-12-28 13:54:59 +0530

    Updates

--- assets/ArrowForwardIos-FazXwDqZ.js
+++
@@ -1,1 +0,0 @@
-import{y as o,j as r}from"./index-DtdiYH6P.js";const t=o(r.jsx("path",{d:"M6.23 20.23 8 22l10-10L8 2 6.23 3.77 14.46 12z"}));export{t as A};
--- assets/ChevronLeft-DWGo09PA.js
+++
@@ -1,1 +0,0 @@
-import{y as o,j as t}from"./index-DtdiYH6P.js";const e=o(t.jsx("path",{d:"M15.41 7.41 14 6l-6 6 6 6 1.41-1.41L10.83 12z"}));export{e as C};
